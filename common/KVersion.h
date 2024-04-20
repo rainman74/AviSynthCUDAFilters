b691f10793cc5808a7a95888acc88052e0b3eb5a
rigaya <rigaya34589@live.jp>
2024-04-20 13:44:54 +0900

    dllのプロパティにバージョン情報を表示するように。

---
+++ common/KVersion.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef __KVERSION_H__
+#define __KVERSION_H__
+
+#define VER_FILEVERSION             0,0,6,0
+#define VER_STR_FILEVERSION          "0.6.0"
+#define VER_STR_FILEVERSION_TCHAR _T("0.6.0")
+
+#endif //__KVERSION_H__
