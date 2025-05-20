223155f6169d4951176eef3b6ee5df00138ebafc
rigaya <rigaya34589@live.jp>
2025-05-21 07:44:58 +0900

    NNEDI3の更新。

--- common/KVersion.h
+++ common/KVersion.h
@@ -2,8 +2,8 @@
 #ifndef __KVERSION_H__
 #define __KVERSION_H__
 
-#define VER_FILEVERSION             0,0,7,0
-#define VER_STR_FILEVERSION          "0.7.0"
-#define VER_STR_FILEVERSION_TCHAR _T("0.7.0")
+#define VER_FILEVERSION             0,0,7,1
+#define VER_STR_FILEVERSION          "0.7.1"
+#define VER_STR_FILEVERSION_TCHAR _T("0.7.1")
 
 #endif //__KVERSION_H__
